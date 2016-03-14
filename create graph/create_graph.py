1c81d6426f72069c00a8dd25b6f9811e95f9adf7
mathooo <trojak@mail.muni.cz>
2016-03-14 19:00:16 +0100

    Fix

--- create graph/create_graph.py
+++ create graph/create_graph.py
@@ -1,125 +1,18 @@
 import sys
 import copy
 
-def first_part(output_file):
-    f = open(output_file, 'w')
-    f.write("<!doctype html>\n")
-    f.write("<html>\n")
-    f.write("<head>\n")
-    f.write("    <title>Network | Interaction events</title>\n")
-    f.write("\n")
-    f.write("    <script type=\"text/javascript\" src=\"/home/matho/node_modules/vis/dist/vis.js\"></script>\n")
-    f.write("    <link href=\"/home/matho/node_modules/vis/dist/vis.css\" rel=\"stylesheet\" type=\"text/css\"/>\n")
-    f.write("\n")
-    f.write("    <style type=\"text/css\">\n")
-    f.write("       #mynetwork {\n")
-    f.write("            width: 800px;\n")
-    f.write("            height: 500px;\n")
-    f.write("            border: 1px solid lightgray;\n")
-    f.write("        }\n")
-    f.write("	    #rectangle {\n")
-    f.write("		    text-align: center;\n")
-    f.write("		    font-weight: bold;\n")
-    f.write("	    }\n")
-    f.write("}\n")
-    f.write("    </style>\n")
-    f.write("</head>\n")
-    f.write("<body>\n")
-    f.write("\n")
-    f.write("<div id=\"mynetwork\"></div>\n")
-    f.write("<div id=\"rectangle\"style=\"width:800px;height:100%;border:1px solid #000;\"> </div>\n")
-    f.write("\n")
-    f.write("<script type=\"text/javascript\">\n")
-    f.write("    // create an array with nodes\n")
-    f.write("        var nodes = new vis.DataSet([\n")
-    f.close()
-
-def second_part(output_file):
-    f = open(output_file, 'a')
-    f.write("	]);\n")
-    f.write("\n")
-    f.write("    // create a network\n")
-    f.write("    var container = document.getElementById('mynetwork');\n")
-    f.write("    var data = {\n")
-    f.write("        nodes: nodes,\n")
-    f.write("        edges: edges\n")
-    f.write("    };\n")
-    f.write("    var options = {\n")
-    f.write("		layout:{randomSeed:2},\n")
-    f.write("		physics: {\n")
-    f.write("			stabilization: false,\n")
-    f.write("			barnesHut: {\n")
-    f.write("				gravitationalConstant: -10000,\n")
-    f.write("				centralGravity: 0.75,\n")
-    f.write("				springConstant: 0.08,\n")
-    f.write("				springLength: 150\n")
-    f.write("			},\n")
-    f.write("			maxVelocity: 146,\n")
-    f.write("            solver: 'barnesHut',\n")
-    f.write("            timestep: 0.50,\n")
-    f.write("            stabilization: {iterations: 150}\n")
-    f.write("        },\n")
-    f.write("		nodes: {\n")
-    f.write("            size: 15,\n")
-    f.write("            font: {\n")
-    f.write("                size: 20\n")
-    f.write("            },\n")
-    f.write("            borderWidth: 2,\n")
-    f.write("			borderWidthSelected: 4\n")
-    f.write("        },\n")
-    f.write("        edges: {\n")
-    f.write("            width: 4,\n")
-    f.write("			selectionWidth: function (width) {return width*2.5;}\n")
-    f.write("        },\n")
-    f.write("		interaction: {\n")
-    f.write("          navigationButtons: true,\n")
-    f.write("          keyboard: true,\n")
-    f.write("		  hover: true,\n")
-    f.write("		  tooltipDelay: 500,\n")
-    f.write("        }\n")
-    f.write("	};\n")
-    f.write("    var network = new vis.Network(container, data, options);\n")
-    f.write("	var stabil = true;\n")
-    f.write("\n")
-    f.write("    network.on(\"click\", function (params) {\n")
-    f.write("        params.event = \"[original event]\";\n")
-    f.write("		var tmp = \" \";\n")
-    f.write("	\n")
-    f.write("	\n")
-    f.write("		for (var i = 1; i <= nodes.length; i++) {\n")
-    f.write("        	if (nodes.get(i).id == params.nodes) {\n")
-    f.write("				tmp = nodes.get(i).text;\n")
-    f.write("			};\n")
-    f.write("    	};\n")
-    f.write("		\n")
-    f.write("		if(params.nodes.length === 0 && params.edges.length > 0) {\n")
-    f.write("			for (var i = 1; i <= edges.length; i++) {\n")
-    f.write("				if (edges.get(i).id == params.edges) {\n")
-    f.write("					tmp = edges.get(i).text;\n")
-    f.write("				};\n")
-    f.write("			};\n")
-    f.write("		};\n")
-    f.write("		\n")
-    f.write("		document.getElementById('rectangle').innerHTML = '<div style=\"width:800px;height:100%;text-align:center;border:0px solid #000;\">' + tmp + '</div>';\n")
-    f.write("    });\n")
-    f.write("	\n")
-    f.write("	network.on(\"stabilized\", function (params) {\n")
-    f.write("	if(stabil) {\n")
-    f.write("		network.fit();\n")
-    f.write("		stabil = false;\n")
-    f.write("	};	\n")
-    f.write("	});\n")
-    f.write("	\n")
-    f.write("	network.on(\"doubleClick\", function (params) {\n")
-    f.write("        params.event = \"[original event]\";\n")
-    f.write("		network.focus(params.nodes);\n")
-    f.write("	});\n")
-    f.write("\n")
-    f.write("</script>\n")
-    f.write("\n")
-    f.write("<script src=\"../../googleAnalytics.js\"></script>\n")
-    f.write("</body>\n")
-    f.write("</html>\n")
+def write_part(me, output_file, start, end, mode):
+    f = open(output_file, mode)
+    fp = open(me)
+    allowed_to_read = False
+    for i, line in enumerate(fp):
+        line = line.rstrip()
+        if start == line:
+            allowed_to_read = True
+        elif end == line:
+            allowed_to_read = False
+        elif allowed_to_read:
+            f.write(line + '\n')
     f.close()
 
 def create_reaction(From, To):
@@ -149,11 +42,12 @@ def create_reaction(From, To):
 
     return " + ".join(new_from), " + ".join(new_to)
 
+me = sys.argv[-4]
 vertices_file = sys.argv[-3]
 edges_file = sys.argv[-2]
 output_file = sys.argv[-1]
 
-first_part(output_file)
+write_part(me, output_file, "FIRST PART START", "FIRST PART END", "w")
 
 output = open(output_file, 'a')
 
@@ -219,4 +113,127 @@ with open(edges_file) as edges:
 
 output.close()
 
-second_part(output_file)
+write_part(me, output_file, "SECOND PART START", "SECOND PART END", "a")
+
+'''
+FIRST PART START
+<!doctype html>
+<html>
+<head>
+    <title>Network | Interaction events</title>
+
+    <script type="text/javascript" src="/home/matho/node_modules/vis/dist/vis.js"></script>
+    <link href="/home/matho/node_modules/vis/dist/vis.css" rel="stylesheet" type="text/css"/>
+
+    <style type="text/css">
+       #mynetwork {
+            width: 800px;
+            height: 500px;
+            border: 1px solid lightgray;
+        }
+	    #rectangle {
+		    text-align: center;
+		    font-weight: bold;
+	    }
+}
+    </style>
+</head>
+<body>
+
+<div id="mynetwork"></div>
+<div id="rectangle"style="width:800px;height:100%;border:1px solid #000;"> </div>
+
+<script type="text/javascript">
+    // create an array with nodes
+        var nodes = new vis.DataSet([
+FIRST PART END
+'''
+
+'''
+SECOND PART START
+	]);
+
+    // create a network
+    var container = document.getElementById('mynetwork');
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {
+		layout:{randomSeed:2},
+		physics: {
+			stabilization: false,
+			barnesHut: {
+				gravitationalConstant: -10000,
+				centralGravity: 0.75,
+				springConstant: 0.08,
+				springLength: 150
+			},
+			maxVelocity: 146,
+            solver: 'barnesHut',
+            timestep: 0.50,
+            stabilization: {iterations: 150}
+        },
+		nodes: {
+            size: 15,
+            font: {
+                size: 20
+            },
+            borderWidth: 2,
+			borderWidthSelected: 4
+        },
+        edges: {
+            width: 4,
+			selectionWidth: function (width) {return width*2.5;}
+        },
+		interaction: {
+          navigationButtons: true,
+          keyboard: true,
+		  hover: true,
+		  tooltipDelay: 500,
+        }
+	};
+    var network = new vis.Network(container, data, options);
+	var stabil = true;
+
+    network.on("click", function (params) {
+        params.event = "[original event]";
+		var tmp = " ";
+
+
+		for (var i = 1; i <= nodes.length; i++) {
+        	if (nodes.get(i).id == params.nodes) {
+				tmp = nodes.get(i).text;
+			};
+    	};
+
+		if(params.nodes.length === 0 && params.edges.length > 0) {
+			for (var i = 1; i <= edges.length; i++) {
+				if (edges.get(i).id == params.edges) {
+					tmp = edges.get(i).text;
+				};
+			};
+		};
+
+		document.getElementById('rectangle').innerHTML = '<div style="width:800px;height:100%;text-align:center;border:0px solid #000;">' + tmp + '</div>';
+    });
+
+	network.on("stabilized", function (params) {
+	if(stabil) {
+		network.fit();
+		stabil = false;
+	};
+	});
+
+	network.on("doubleClick", function (params) {
+        params.event = "[original event]";
+		network.focus(params.nodes);
+	});
+
+</script>
+
+<script src="../../googleAnalytics.js"></script>
+</body>
+</html>
+SECOND PART END
+'''
